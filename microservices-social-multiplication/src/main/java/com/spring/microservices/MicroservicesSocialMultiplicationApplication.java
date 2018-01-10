6e67a82a7b2898f385f17286e378161311fa69bb
rahiakela <rahi.akela@yahoo.com>
2018-01-10 17:05:57 +0530

    adding microservices-social-multiplication branch

---
+++ microservices-social-multiplication/src/main/java/com/spring/microservices/MicroservicesSocialMultiplicationApplication.java
@@ -0,0 +1,12 @@
+package com.spring.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSocialMultiplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesSocialMultiplicationApplication.class, args);
+	}
+}
